a042110a7d3542d0de957ed2847768dc4c439f0e
Luke Edwards <luke.edwards05@gmail.com>
2024-08-19 21:09:03 -0700

    chore: download hrana-v3 spec

---
+++ scripts/spec.ts
@@ -0,0 +1,8 @@
+const INPUT = 'https://raw.githubusercontent.com/tursodatabase/libsql/main/docs/HRANA_3_SPEC.md';
+const OUTPUT = 'hrana-v3.md';
+
+let r = await fetch(INPUT);
+if (!r.ok) throw new Error(`[${r.status}] ${await r.text()}`);
+
+await Deno.writeFile(OUTPUT, await r.bytes());
+console.log('+', OUTPUT);

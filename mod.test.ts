bd4e0e433d36e9c50e2ae214299c8e238dd9cc58
Luke Edwards <luke.edwards05@gmail.com>
2024-08-24 12:51:29 -0700

    chore: add `parse` tests

--- mod.test.ts
+++ mod.test.ts
@@ -3,7 +3,7 @@ import { encodeBase64 } from 'jsr:@std/encoding@^1/base64';
 import { encodeBase64Url } from 'jsr:@std/encoding@^1/base64url';
 
 import * as mod from './mod.ts';
-import type { Value } from './hrana.ts';
+import type { StmtResult, Value } from './hrana.ts';
 
 Deno.test('value', async (t) => {
 	function run(expect: unknown, input: Value, mode?: mod.Mode) {
@@ -81,5 +81,56 @@ Deno.test('value', async (t) => {
 			type: 'integer',
 			value: '123',
 		}, 'bigint');
+	});
+});
+
+Deno.test('parse', async (t) => {
+	// select 1
+	let result: StmtResult = {
+		cols: [{ name: '1', decltype: null }],
+		rows: [[{ type: 'integer', value: '1' }]],
+		affected_row_count: 0,
+		last_insert_rowid: null,
+		rows_read: 0,
+		rows_written: 0,
+		query_duration_ms: 0.039,
+	};
+
+	await t.step('simple', () => {
+		let output = mod.parse(result);
+		assertEquals(output, [{ 1: 1 }]);
+	});
+
+	await t.step('simple w/ mode', () => {
+		let output = mod.parse(result, 'bigint');
+		assertEquals(output, [{ 1: 1n }]);
+	});
+
+	await t.step('multiple columns', () => {
+		result.cols = [{
+			name: 'name',
+			decltype: null,
+		}, {
+			name: 'age',
+			decltype: null,
+		}];
+
+		result.rows = [
+			[{ type: 'text', value: 'lukeed' }, { type: 'float', value: 34.5 }],
+			[{ type: 'text', value: 'foobar' }, { type: 'text', value: 'unknown' }],
+			[{ type: 'text', value: 'anon' }, { type: 'null' }],
+		];
+
+		let output = mod.parse(result);
+		assertEquals(output, [{
+			name: 'lukeed',
+			age: 34.5,
+		}, {
+			name: 'foobar',
+			age: 'unknown',
+		}, {
+			name: 'anon',
+			age: null,
+		}]);
 	});
 });
